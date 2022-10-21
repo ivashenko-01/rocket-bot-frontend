299a6d924242bbe3a1d3a621f86eda0c3601d50c
ivashenko-01 <ivashenko-01@mail.ru>
2022-10-21 15:35:43 +0300

    fix

---
+++ src/pages/Office/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Post } from "../components/Post";
+import { Index } from "../components/AddComment";
+import { CommentsBlock } from "../components/CommentsBlock";
+import axios from "../axios";
+import ReactMarkdown from "react-markdown";
+
+export const FullPost = () => {
+
+  const [data, setData] = React.useState();
+  const [isLoading, setLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(()=>{
+    axios
+      .get(`/office`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false)
+      }).catch(err => {
+        console.warn(err);
+        alert('Ошибка при получении данных')
+      })
+  }, [])
+
+  if(isLoading) {
+    return <Post isLoading={isLoading} isFullPost/>
+  }
+
+  return (
+    <>
+      <CommentsBlock
+        items={[
+          {
+            user: {
+              fullName: "Вася Пупкин",
+              avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+            },
+            text: "Это тестовый комментарий 555555",
+          },
+          {
+            user: {
+              fullName: "Иван Иванов",
+              avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+            },
+            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+          },
+        ]}
+        isLoading={false}
+      >
+        <Index />
+      </CommentsBlock>
+    </>
+  );
+};
