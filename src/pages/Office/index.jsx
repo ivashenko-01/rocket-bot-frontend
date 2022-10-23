8e89942ce6a412c3e7c30ac55fb3cacb1082e95a
ivashenko-01 <ivashenko-01@mail.ru>
2022-10-23 11:52:19 +0300

    fix

--- src/pages/Office/index.jsx
+++ src/pages/Office/index.jsx
@@ -36,7 +36,6 @@ export const Office = () => {
     }
   }
 
-
   if(!isAuth) {
     return <Navigate to="/" />;
   }
@@ -44,45 +43,11 @@ export const Office = () => {
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
+        В разработке
       </Typography>
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField         
-        error = {Boolean(errors.fullName?.message)}
-        helperText= {errors.fullName?.message}
-        {...register('fullName', {required: 'Укажите полное имя'})}
-        className={styles.field} 
-        label="Полное имя" 
-        fullWidth 
-      />
-
-      <TextField         
-        error = {Boolean(errors.email?.message)}
-        helperText= {errors.email?.message}
-        type="email"
-        {...register('email', {required: 'Укажите почту'})}
-        className={styles.field} 
-        label="E-Mail" 
-        fullWidth 
-      />
-
-      <TextField         
-        error = {Boolean(errors.password?.message)}
-        helperText= {errors.password?.message}
-        {...register('password', {required: 'Укажите пароль'})}
-        className={styles.field} 
-        label="Пароль" 
-        fullWidth 
-      />
-          
-      <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
-        Зарегистрироваться
-      </Button>
-    </form>
   </Paper>
   );
 };
